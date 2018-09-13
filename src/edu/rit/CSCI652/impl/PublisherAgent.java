adaeadcbeec4711d7559788d93b437ca73db4796
Bharath Suresh Modhipalli <bm5937@rit.edu>
2018-09-13 12:36:04 -0400

    Created separate classes for PublisherAgent and SubscriberAgent

---
+++ src/edu/rit/CSCI652/impl/PublisherAgent.java
@@ -0,0 +1,17 @@
+package edu.rit.CSCI652.impl;
+
+import edu.rit.CSCI652.demo.Event;
+import edu.rit.CSCI652.demo.Publisher;
+import edu.rit.CSCI652.demo.Topic;
+
+public class PublisherAgent implements Publisher {
+    @Override
+    public void publish(Event event) {
+
+    }
+
+    @Override
+    public void advertise(Topic newTopic) {
+
+    }
+}
---
+++ src/edu/rit/CSCI652/impl/SubscriberAgent.java
@@ -0,0 +1,34 @@
+package edu.rit.CSCI652.impl;
+
+import edu.rit.CSCI652.demo.Event;
+import edu.rit.CSCI652.demo.Subscriber;
+import edu.rit.CSCI652.demo.Topic;
+
+
+public class SubscriberAgent implements Subscriber {
+
+    @Override
+    public void subscribe(Topic topic) {
+
+    }
+
+    @Override
+    public void subscribe(String keyword) {
+
+    }
+
+    @Override
+    public void unsubscribe(Topic topic) {
+
+    }
+
+    @Override
+    public void unsubscribe() {
+
+    }
+
+    @Override
+    public void listSubscribedTopics() {
+
+    }
+}
