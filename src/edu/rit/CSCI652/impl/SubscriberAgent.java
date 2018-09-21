71efb97852930585ae6b440d8b5ab726207bfb71
Pranav Mohan Rane <pmr5279@g.rit.edu>
2018-09-21 21:47:31 +0000

    New Command Line Interface, Output Cleanup

--- src/edu/rit/CSCI652/impl/SubscriberAgent.java
+++ src/edu/rit/CSCI652/impl/SubscriberAgent.java
@@ -4,6 +4,7 @@ import edu.rit.CSCI652.demo.Event;
 import edu.rit.CSCI652.demo.Subscriber;
 import edu.rit.CSCI652.demo.Topic;
 
+import javax.annotation.processing.SupportedSourceVersion;
 import javax.rmi.ssl.SslRMIClientSocketFactory;
 import java.io.IOException;
 import java.io.ObjectInputStream;
@@ -18,8 +19,10 @@ import java.util.Vector;
 
 public class SubscriberAgent extends Thread implements Subscriber {
     private ServerSocket ss = null;
-    static int numberOfThreads = 2;
-    private int port = 11001;
+    static int numberOfThreads = 0;
+    private int listeningPort = 0;
+    private String sendingAddress = "";
+    private int sendingPort = 0;
     static private Vector<Topic> topicList = new Vector<Topic>();
 
     public SubscriberAgent() {
@@ -30,9 +33,51 @@ public class SubscriberAgent extends Thread implements Subscriber {
         this.ss = ss;
     }
 
+    public void setAddresses(int listeningPort, int threadCount,
+                             String sendingAddress, int sendingPort){
+
+        this.listeningPort = listeningPort;
+        numberOfThreads = threadCount;
+        this.sendingAddress = sendingAddress;
+        this.sendingPort = sendingPort;
+    }
+
+    public void printAddresses(){
+        System.out.println("\tListening Port: " + listeningPort);
+        System.out.println("\tThread Count: " + numberOfThreads);
+        System.out.println("\tSending Address: " + sendingAddress);
+        System.out.println("\tSending Port: " + sendingPort);
+    }
+
+    public void printTopicVectors(Vector<Topic> list){
+
+        if(list.size() > 0){
+            for(Topic topic: list){
+                topic.printAllVariables();
+                //System.out.println();
+            }
+        }
+        else {
+            System.out.println("\tNo Topic Available");
+        }
+    }
+
+    public void printEventVectors(Vector<Event> list){
+
+        if(list!=null){
+            for(Event topic: list){
+                topic.printAllVariables();
+                System.out.println();
+            }
+        }
+        else {
+            System.out.println("No Pending Events Available at this moment");
+        }
+    }
+
     public void startService() {
         try {
-            ss = new ServerSocket(port);
+            ss = new ServerSocket(listeningPort);
             SubscriberAgent pub = new SubscriberAgent(ss);
             pub.start();
 
@@ -56,17 +101,18 @@ public class SubscriberAgent extends Thread implements Subscriber {
                     // Topic newTopic = (Topic) in.readObject();
                     Topic newTopic = (Topic) in.readObject();
                     topicList.add(newTopic);
-                    System.out.println("Received new topic: " +
+                    System.out.println("Received new topic named: " +
                             newTopic.getName());
                     displayTopicWithNumbers();
                 }
                 // 1000 -> Receiving topics list and pending events if any.
                 else if (code == 1000) {
                     topicList = (Vector<Topic>)in.readObject();
-                    System.out.println("Received topics list: " + topicList);
+                    System.out.println("Received topics list: ");
+                    printTopicVectors(topicList);
                     Vector<Event> pendingEvents = (Vector<Event>) in.readObject();
-                    System.out.println("Received pending events: " +
-                            pendingEvents);
+                    System.out.println("Received pending events: ");
+                    printEventVectors(pendingEvents);
                 }
                 // 4-> Receiving subscribed topic list
                 else if (code == 4){
@@ -77,17 +123,18 @@ public class SubscriberAgent extends Thread implements Subscriber {
                         System.out.println("No Topics Subscribed");
                     }
                     else {
-                        System.out.println("Received Subscribed Topic List");
+                        System.out.println("The Subscribed Topic List is:");
+                        this.printList(subscribedTopicsList);
                     }
-                    this.printList(subscribedTopicsList);
+
 
                 }
                 // 3-> Receiving Events
                 else if (code == 3){
-                    System.out.println("code" + 3);
                     Event newEvent = (Event) in.readObject();
                     System.out.println("Received New Event: "
                             + newEvent.getTitle());
+                    newEvent.printAllVariables();
                 }
             } catch (Exception e) {
                 System.out.println("Error Being Handled");
@@ -108,7 +155,7 @@ public class SubscriberAgent extends Thread implements Subscriber {
             // 2 -> subscribe to topic
             out.writeInt(2);
             out.writeUTF(Inet4Address.getLocalHost().getHostAddress() +
-                    ":" + this.port);
+                    ":" + this.listeningPort);
             out.writeObject(topic);
             out.flush();
         } catch (IOException e) {
@@ -136,17 +183,17 @@ public class SubscriberAgent extends Thread implements Subscriber {
 
     @Override
     public void unsubscribe(Topic topic) {
-        System.out.println("unsubscribe:topic");
+        // System.out.println("unsubscribe:topic");
         Socket clientSocket = null;
         try {
-            System.out.println("Unsubscribing topic: " + topic.getName());
+            System.out.println("Unsubscribing from topic: " + topic.getName());
             clientSocket = new Socket("localhost", 4444);
             ObjectOutputStream out = new ObjectOutputStream(clientSocket
                     .getOutputStream());
             // 6 -> unsubscribe a topic
             out.writeInt(6);
             out.writeUTF(Inet4Address.getLocalHost().getHostAddress() +
-                    ":" + this.port);
+                    ":" + this.listeningPort);
             out.writeObject(topic);
             out.flush();
         } catch (IOException e) {
@@ -158,17 +205,17 @@ public class SubscriberAgent extends Thread implements Subscriber {
 
     @Override
     public void unsubscribe() {
-        System.out.println("unsubscribe");
+        // System.out.println("unsubscribe");
         Socket clientSocket = null;
         try {
-            System.out.println("Unsubscribing all topics ");
-            clientSocket = new Socket("localhost", 4444);
+            System.out.println("Unsubscribing from all topics ");
+            clientSocket = new Socket(sendingAddress, sendingPort);
             ObjectOutputStream out = new ObjectOutputStream(clientSocket
                     .getOutputStream());
             // 5 -> Unsubscribe All
             out.writeInt(5);
             out.writeUTF(Inet4Address.getLocalHost().getHostAddress() +
-                    ":" + this.port);
+                    ":" + this.listeningPort);
             out.flush();
         } catch (IOException e) {
             e.printStackTrace();
@@ -180,13 +227,13 @@ public class SubscriberAgent extends Thread implements Subscriber {
         Socket clientSocket = null;
         try {
             System.out.println("Getting Subscribed Topics ");
-            clientSocket = new Socket("localhost", 4444);
+            clientSocket = new Socket(sendingAddress, sendingPort);
             ObjectOutputStream out = new ObjectOutputStream(clientSocket
                     .getOutputStream());
             // 4 -> supply subscribed topic list for Subscriber
             out.writeInt(4);
             out.writeUTF(Inet4Address.getLocalHost().getHostAddress() +
-                    ":" + this.port);
+                    ":" + this.listeningPort);
             out.flush();
 
             ObjectInputStream in = new ObjectInputStream(clientSocket.getInputStream());
@@ -207,14 +254,25 @@ public class SubscriberAgent extends Thread implements Subscriber {
     public void listSubscribedTopics() {
         // System.out.println("listSubscribedTopics");
         Vector<String> subscribedTopicsList = this.getSubscribedTopics();
-        if (subscribedTopicsList.size() == 0){
-            System.out.println("No Topics Subscribed");
-        }
-        else {
-            System.out.println("Received Subscribed Topic List");
-        }
-        this.printList(subscribedTopicsList);
 
+        try {
+            if(subscribedTopicsList == null){
+                System.out.println("Subscriber Removed from Server");
+                System.out.println("Pinging to Register...");
+                this.ping();
+                System.out.println("No Topics Subscribed");
+            }
+            else if (subscribedTopicsList.size() == 0){
+                System.out.println("No Topics Subscribed");
+            }
+            else {
+                System.out.println("Received Subscribed Topic List");
+                this.printList(subscribedTopicsList);
+            }
+        }
+        catch (Exception e){
+            e.printStackTrace();
+        }
     }
 
     /*Connect and register network*/
@@ -222,13 +280,13 @@ public class SubscriberAgent extends Thread implements Subscriber {
     public void ping() {
         Socket clientSocket = null;
         try {
-            System.out.println("Pinging.");
-            clientSocket = new Socket("localhost", 4444);
+            System.out.println("Pinging...");
+            clientSocket = new Socket(sendingAddress, sendingPort);
             ObjectOutputStream out = new ObjectOutputStream(clientSocket
                     .getOutputStream());
             out.writeInt(1000);
             out.writeUTF(Inet4Address.getLocalHost().getHostAddress() +
-                    ":" + this.port);
+                    ":" + this.listeningPort);
             out.flush();
         } catch (IOException e) {
             e.printStackTrace();
@@ -241,27 +299,60 @@ public class SubscriberAgent extends Thread implements Subscriber {
         // System.out.println("printList");
         int i = 0;
         for(String content : contentList) {
-            System.out.println("" + i + ":" + content);
+            System.out.println("\t" + i + ":" + content);
             i++;
         }
     }
 
     /*Displays all the topics ever registered with event manager*/
     private void displayTopicWithNumbers (){
-        if(topicList.size() == 0) System.out.println("No Topics Present");
-        for (int i=0; i<topicList.size(); i++){
-            System.out.println(i + ":" + topicList.get(i).getName());
+        if(topicList.size() == 0) {
+            System.out.println("No Topics Present");
+        }
+        else {
+            System.out.println("The Topics Available are:");
+            for (int i=0; i<topicList.size(); i++){
+                System.out.println("\t" + i + ":" + topicList.get(i).getName());
+            }
         }
     }
 
     /*Display KeyWords for every topic*/
     private void displayKeyWordsForTopics (){
-        for (Topic consideration: topicList) consideration.printKeyWords();
+        for (Topic consideration: topicList)
+        {
+            consideration.printKeyWordsWithTopicName();
+        }
     }
 
     public static void main(String[] args) throws InterruptedException {
 
         SubscriberAgent subUI = new SubscriberAgent();
+
+        if (args.length==8 && args[0].equals("-port") &&
+                args[2].equals("-threads") &&
+                args[4].equals("-eventManagerIP") &&
+                args[6].equals("-eventManagerPort")) {
+            try {
+                System.out.println("Setting Connection Variables:");
+                subUI.setAddresses(Integer.parseInt(args[1]),
+                        Integer.parseInt(args[3]), args[5],
+                        Integer.parseInt(args[7]));
+                subUI.printAddresses();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+        else {
+            System.err.println("Enter Command Like this: " +
+                    "SubscriberAgent -port 11001 -threads 2 " +
+                    "-eventManagerIP localhost -eventManagerPort 4444");
+            System.out.println("Using Default Values");
+            subUI.setAddresses(11001, 2,
+                    "localhost", 4444);
+            subUI.printAddresses();
+        }
+
         subUI.startService();
         Scanner sc = new Scanner(System.in);
         int userInput = 0;
@@ -280,7 +371,7 @@ public class SubscriberAgent extends Thread implements Subscriber {
                 System.out.println("Press 6 to Ping");
                 System.out.println("Press 7 to view topics");
                 System.out.println("Press 8 to Exit the loop");
-                System.out.print("Please select an option: ");
+                System.out.println("Please select an option: ");
 
                 userInput = sc.nextInt();
 
@@ -292,7 +383,7 @@ public class SubscriberAgent extends Thread implements Subscriber {
                         subUI.subscribe(topicList.get(topicId0));
                         break;
                     case 2:
-                        System.out.println("Select a keyword from list displayed " +
+                        System.out.println("Write a keyword from list displayed " +
                                 "below");
                         subUI.displayKeyWordsForTopics();
                         String keyWordValue = sc.next();
